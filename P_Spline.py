742fbe938f4dfed191269eb9052865b51f7976a1
Nishant Kumar <31392545+nish91@users.noreply.github.com>
2017-10-04 19:50:53 +0200

    Updated P_Spline.py
    Code now compatible with Taurus machine. Successfully generates spline coefficients for 307200 pixel with 0.0001 as smoothing parameter and 200 knots.

--- P_Spline.py
+++ P_Spline.py
@@ -4,16 +4,18 @@ Created on Thu Aug 15 12:19:36 2017
 
 @author: Nishant
 """
-import numpy as np
-import scipy as sp
+import numpy as numpy
+import scipy as scipy
 from copy import deepcopy
 import h5py
-import matplotlib.pyplot as plt
+import matplotlib.pyplot 
+import multiprocessing
 
+multiprocessing.cpu_count()
 
 """import data"""
-f = h5py.File("D:/Tu dresden/4th Sem/Present/After literature review/Datasets/sep_1072240.mat", "r")
-img = np.array(f["img"].value)
+f = h5py.File("sep_1072240.mat", "r")
+img = numpy.array(f["img"].value)
 f.close()
 
 """Response variable Y = 1066*307200"""
@@ -23,36 +25,36 @@ Y =  img.reshape((img.shape[0], -1))
 del img
 
 """define observation variable X = 1066 points"""
-X = np.linspace(0,1065,1066)
+X = numpy.linspace(0,1065,1066)
 
 """define number of splines and order of splines"""
 no_of_splines = 200
 order_of_spline = 3
 
 """define knots"""
-knots = np.linspace(0, 1, 1 + no_of_splines - order_of_spline) #Return evenly spaced numbers over a specified interval.
-difference = np.diff(knots[:2])[0]   #difference of first two knots
+knots = numpy.linspace(0, 1, 1 + no_of_splines - order_of_spline) #Return evenly spaced numbers over a specified interval.
+difference = numpy.diff(knots[:2])[0]   #difference of first two knots
 
 
 """scale the values of X to be in the range of 0 & 1 by normalization"""
-x = (np.ravel(deepcopy(X)) - X[0]) / float(X[-1] - X[0]) # x = np.array([[1, 2, 3], [4, 5, 6]]) print(np.ravel(x)) gives [1 2 3 4 5 6]
+x = (numpy.ravel(deepcopy(X)) - X[0]) / float(X[-1] - X[0]) # x = numpy.array([[1, 2, 3], [4, 5, 6]]) print(numpy.ravel(x)) gives [1 2 3 4 5 6]
 
 """delete X since saves memory"""
 del X
-x = np.r_[x, 0., 1.] # append 0 and 1 in order to get derivatives for extrapolation
-x = np.r_[x] #np.r_[np.array([1,2,3]), 0, 0, np.array([4,5,6])] gives array([1, 2, 3, 0, 0, 4, 5, 6])  append 0 and 1 in order to get derivatives for extrapolation
-x = np.atleast_2d(x).T #View inputs as arrays with at least two dimensions
+x = numpy.r_[x, 0., 1.] # append 0 and 1 in order to get derivatives for extrapolation
+x = numpy.r_[x] #numpy.r_[numpy.array([1,2,3]), 0, 0, numpy.array([4,5,6])] gives array([1, 2, 3, 0, 0, 4, 5, 6])  append 0 and 1 in order to get derivatives for extrapolation
+x = numpy.atleast_2d(x).T #View inputs as arrays with at least two dimensions
 n = len(x)
 
 
 """define corner knots on left and right of original knots"""
-corner = np.arange(1, order_of_spline + 1) * difference
-new_knots = np.r_[-corner[::-1], knots, 1 + corner]
+corner = numpy.arange(1, order_of_spline + 1) * difference
+new_knots = numpy.r_[-corner[::-1], knots, 1 + corner]
 new_knots[-1] += 1e-9 # want last knot inclusive
 
 
 """ prepare Haar Basis""" 
-basis = (x >= new_knots[:-1]).astype(np.int) * (x < new_knots[1:]).astype(np.int)
+basis = (x >= new_knots[:-1]).astype(numpy.int) * (x < new_knots[1:]).astype(numpy.int)
 basis[-1] = basis[-2][::-1] # force symmetric bases at 0 and 1
 
 """ De-boor recursion"""
@@ -65,12 +67,12 @@ for m in range(2, order_of_spline + 2):
     """ left sub-basis function"""
     left_numerator = (x - new_knots[:maxi][mask_l]) * basis[:, :maxi][:, mask_l]
     left_denominator = new_knots[m-1 : maxi+m-1][mask_l] - new_knots[:maxi][mask_l]
-    left = np.zeros((n, maxi))
+    left = numpy.zeros((n, maxi))
     left[:, mask_l] = left_numerator/left_denominator
     """ right sub-basis function"""
     right_numerator = (new_knots[m : maxi+m][mask_r]-x) * basis[:, 1:maxi+1][:, mask_r]
     right_denominator = new_knots[m:maxi+m][mask_r] - new_knots[1 : maxi+1][mask_r]
-    right = np.zeros((n, maxi))
+    right = numpy.zeros((n, maxi))
     right[:, mask_r] = right_numerator/right_denominator
     """ track previous bases and update"""
     prev_bases = basis[-2:]
@@ -80,12 +82,13 @@ for m in range(2, order_of_spline + 2):
 """finally create a sparse basis matrix in compressed sparse column format"""
 """helpful in arithmetic operations, saves memory"""
 basis = basis[:-2]     # get rid of the added values at 0, and 1
-basis_matrix = sp.sparse.csc_matrix(basis)
+import scipy.sparse
+basis_matrix = scipy.sparse.csc_matrix(basis)
 
 """delete unrequired variables to save memory"""
 del basis,corner, difference, knots, left, left_denominator, left_numerator,  x
 del m, mask_l, mask_r, maxi, n, new_knots, order_of_spline, prev_bases, right, right_denominator, right_numerator
-
+import scipy.sparse.linalg
 """"""""""""""""""""""""""""""""""""""
 """Now estimate spline coefficients"""
 """"""""""""""""""""""""""""""""""""""
@@ -97,10 +100,10 @@ coefficients_pixels = []
 lambda_param = [0.0001]  
 """create and initialize penalty matrix and multiply the matrix with lambda_param"""
 Ps = []
-p = sp.sparse.eye(no_of_splines).tocsc()
+p = scipy.sparse.eye(no_of_splines).tocsc()
 Ps.append(p)
-Penalty_matrix = tuple([np.multiply(P, lam) for lam, P in zip(lambda_param, Ps)])
-Penalty_matrix = sp.sparse.block_diag(Penalty_matrix)
+Penalty_matrix = tuple([numpy.multiply(P, lam) for lam, P in zip(lambda_param, Ps)])
+Penalty_matrix = scipy.sparse.block_diag(Penalty_matrix)
 Penalty_matrix = Penalty_matrix.todense()
 """delete unrequired variables to save memory"""
 del lam,lambda_param,Ps,p 
@@ -108,36 +111,36 @@ del lam,lambda_param,Ps,p
 """ decomposes positive-definite matrix into the product of lower triangular matrix""" 
 """and its conjugate transpose, for efficient numerical solutions, saving computation time and storage space and to increase regularisation""" 
 """regularization. Test for positive definite: gaussianwaves.com/2013/04/tests-for-positive-definiteness-of-a-matrix/ """
-cholesky_matrix = np.linalg.cholesky(Penalty_matrix) #Cholesky decomposition.
+cholesky_matrix = numpy.linalg.cholesky(Penalty_matrix) #Cholesky decomposition.
 del Penalty_matrix, no_of_splines
  
 """following is the iteration for each pixel to determine the coefficient
     for each pixels till 307200 pixels,link_function = 'identity', distribution = 'normal'  """
 for i in range(307200):
     #initialise coefficients m =200(splines) n=1066(time points) no_splines = 200 min_n_m = 200"""
-    coefficients = np.ones((200,1)) * np.sqrt(np.finfo(np.float64).eps) #is all together then coefficient will be 200*307200
+    coefficients = numpy.ones((200,1)) * numpy.sqrt(numpy.finfo(numpy.float64).eps) #is all together then coefficient will be 200*307200
     #Initialise inverse of a diagonal matrix (D_inv). D is a Diagonal matrix with singular values"""
     #singular values are eigenvalues/eigenvectors 1<= value <= min(basis_matrix.shape) hence taking highest value which is 1064
-    D_inverse = np.zeros((199, 199)).T
+    D_inverse = numpy.zeros((199, 199)).T
     #Define and initialise weights with ones for each response"""
-    weights = np.ones_like(Y[:,i]).astype('f') #replaces values in Y[:,i] by 1. [123] becomes [111]
+    weights = numpy.ones_like(Y[:,0]).astype('f') #replaces values in Y[:,i] by 1. [123] becomes [111]
     #multiply the model matrix by the spline basis coefficients (bZ)
     linear_predictor = basis_matrix.dot(coefficients).flatten()
     #create weight matrix
-    W = sp.sparse.diags((np.ones_like(linear_predictor)**2 * np.ones_like(linear_predictor) * weights ** -1)**-0.5)
+    W = scipy.sparse.diags((numpy.ones_like(linear_predictor)**2 * numpy.ones_like(linear_predictor) * weights ** -1)**-0.5)
     #pseudo data for iterations
-    pseudo_data = W.dot(linear_predictor + (Y[:,i] - linear_predictor) * np.ones_like(linear_predictor))
+    pseudo_data = W.dot(linear_predictor + (Y[:,i] - linear_predictor) * numpy.ones_like(linear_predictor))
     # common matrix product
     WB = weights.dot(basis_matrix)
     #QR decomposition
-    Q, R = np.linalg.qr(WB[0].todense()) 
+    Q, R = numpy.linalg.qr(WB[0].todense()) 
     #singular value decomposition
-    #U, D, Vt = np.linalg.svd(np.vstack([R, cholesky_matrix.T])) , k=eigenvalues
-    U, D, Vt = sp.sparse.linalg.svds(sp.sparse.vstack([sp.sparse.csc_matrix(R), sp.sparse.csc_matrix(cholesky_matrix.T)]),k=199)
+    #U, D, Vt = numpy.linalg.svd(numpy.vstack([R, cholesky_matrix.T])) , k=eigenvalues
+    U, D, Vt = scipy.sparse.linalg.svds(scipy.sparse.vstack([scipy.sparse.csc_matrix(R), scipy.sparse.csc_matrix(cholesky_matrix.T)]),k=199)
     # mask out small singular values (RANK deficiency taken care of)
-    svd_mask = D <= (D.max() * np.sqrt(np.finfo(np.float64).eps))
+    svd_mask = D <= (D.max() * numpy.sqrt(numpy.finfo(numpy.float64).eps))
     # invert the singular values and fill the D_inverse as D^-1
-    np.fill_diagonal(D_inverse, D**-1)
+    numpy.fill_diagonal(D_inverse, D**-1)
     # keep only top portion of U
     U1 = U[:200,:]
     B = Vt.T.dot(D_inverse).dot(U1.T).dot(Q.T)
@@ -149,23 +152,22 @@ for i in range(307200):
 del D_inverse,weights,linear_predictor,W,pseudo_data,WB,Q,R,U,D,Vt,svd_mask,U1,B,coefficients,cholesky_matrix,i,coefficients_new
 
 """convert coefficients_pixels from a list to numpy array"""
-coefficients_pixels = np.asarray(coefficients_pixels)
+coefficients_pixels = numpy.asarray(coefficients_pixels)
 
 """Convert sparse basis matrix to dense basis matrix"""
 basis_matrix = basis_matrix.todense()
 
 """Estimate of Y_hat"""
 Y_hat = coefficients_pixels.dot(basis_matrix.T)
-Y_hat = np.squeeze(np.asarray(Y_hat))
+Y_hat = numpy.squeeze(numpy.asarray(Y_hat))
 
-"""Verification of p-splines"""
-x = np.linspace(0, 1065, 1066)
-""" Plot the data """
-plt.figure(figsize=(10,8))
-plt.scatter(x, Y[:,0], label = 'Original Y', marker = 'o', s=10)
-plt.plot(np.delete(x,[0,1065]), np.delete(Y_hat,[0,1065]), label = 'Estimate Y', linewidth =1.0 ,color='r')
-plt.xlabel(r'Time points (X)',fontweight='bold',fontsize=10)
-plt.ylabel(r'Response variable (Y)',fontweight='bold', fontsize=10)
-plt.legend(prop={'size': 12})
-plt.title(r'$\lambda = 0.0001$, # knots = 200', fontsize=15)
-plt.show()
+"""Verification of fitting of p-splines """
+x = numpy.linspace(0, 1065, 1066)
+matplotlib.pyplot.figure(figsize=(10,8))
+matplotlib.pyplot.scatter(x, Y[:,0], label = 'Original Y', marker = 'o', s=10)
+matplotlib.pyplot.plot(numpy.delete(x,[0,1065]), numpy.delete(Y_hat,[0,1065]), label = 'Estimate Y', linewidth =1.0 ,color='r')
+matplotlib.pyplot.xlabel(r'Time points (X)',fontweight='bold',fontsize=10)
+matplotlib.pyplot.ylabel(r'Response variable (Y)',fontweight='bold', fontsize=10)
+matplotlib.pyplot.legend(prop={'size': 12})
+matplotlib.pyplot.title(r'$\lambda = 0.0001$, # knots = 200', fontsize=15)
+matplotlib.pyplot.show()
