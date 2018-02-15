5d081913d0b9c1fb482b13fbc6b276cecab03e56
nish03 <31392545+nish03@users.noreply.github.com>
2018-02-15 15:39:53 +0100

    Updated pixel_mrf_model.py
    Changes in accordance with the testing of single non-parametric component with MRF smoother

--- semiparamRegression/pixel_mrf_model.py
+++ semiparamRegression/pixel_mrf_model.py
@@ -3,25 +3,25 @@ from sklearn.decomposition import PCA
 from math import sqrt
 from sklearn import mixture
 import opengm
+from sklearn.preprocessing import StandardScaler
 
 def fast_norm(x):
     return sqrt(x.dot(x.conj()))
 
-def pixel_mrf_model(num_knots,beta_mrf,S2,B2,noPixels):
-    #taking principal components
+def pixel_mrf_model(num_knots,beta,S2,B,noPixels):
+    #taking principal components   
     pca = PCA(n_components=num_knots)
-    pca.fit(beta_mrf.T)
-    var1= numpy.cumsum(numpy.round(pca.explained_variance_ratio_, decimals=4)*100)
+    pca.fit(beta.T)
+    var1= numpy.cumsum(numpy.round(pca.explained_variance_ratio_, decimals=3)*100)
     components = numpy.argmax(numpy.unique(var1)) + 1
     pca = PCA(n_components=components)
-    pca.fit(beta_mrf.T)
+    pca.fit(beta.T)
     eigenvector_matrix = pca.components_
-    beta_mrf = beta_mrf.T.dot(eigenvector_matrix.T)
+    beta = beta.T.dot(eigenvector_matrix.T)
     #discretization
-    num_clusters = 10
+    num_clusters = 5
     gmm = mixture.GaussianMixture(n_components=num_clusters)
-    gmm.fit(beta_mrf)
-    labels = gmm.predict(beta_mrf)
+    gmm.fit(beta)
     means  = gmm.means_
     #means for mrf
     means_inv_PCA = eigenvector_matrix.T.dot(means.T)
@@ -31,7 +31,7 @@ def pixel_mrf_model(num_knots,beta_mrf,S2,B2,noPixels):
     pixel_unaries = numpy.zeros((n_pixels,n_labels_pixels),dtype=numpy.float32)
     for i in range(n_pixels):
         for l in range(n_labels_pixels):
-            pixel_unaries[i,l] = fast_norm(S2[:,i] - B2.T.dot(means_inv_PCA[:,l])) #L2 norm
+            pixel_unaries[i,l] = fast_norm(S2[:,i] - B.T.dot(means_inv_PCA[:,l])) #L2 norm
     #define pixel regularizer
     pixel_regularizer = opengm.differenceFunction(shape=[n_labels_pixels,n_labels_pixels],norm=1,weight=1.0/n_labels_pixels,truncate=None)
     #initialise graphical model
@@ -53,5 +53,5 @@ def pixel_mrf_model(num_knots,beta_mrf,S2,B2,noPixels):
     centroid_labels = [means[i,:] for i in argmin]
     centroid_labels = numpy.asarray(centroid_labels)
     #inverse PCA
-    beta_mrf = eigenvector_matrix.T.dot(centroid_labels.T)
-    return beta_mrf
+    beta = eigenvector_matrix.T.dot(centroid_labels.T)
+    return beta
