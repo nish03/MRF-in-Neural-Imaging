211a36f2accc03ce6fee54785041b6d5561d21f1
nish03 <31392545+nish03@users.noreply.github.com>
2018-02-27 19:00:58 +0100

    Updated ThermalImagingAnalysis.py
    Refitted the parametric component after spatial regularisation

--- semiparamRegression/ThermalImagingAnalysis.py
+++ semiparamRegression/ThermalImagingAnalysis.py
@@ -5,15 +5,13 @@ import scipy.linalg as linalg
 import matplotlib.pyplot as plt
 import pixel_mrf_model as pm
 
-
-def semiparamRegressionRaw(S2, X, B, P):
+def semiparamRegression(S2, X, B, P, num_knots,num_clusters, noPixels):
     """Apply semiparametric regression framework to imaging data.
     S: m x n data cube with m time series of length n
     X: length m vector of discretized parametric function
     B: non parametric basis
     P: penalty matrix of non-parametric basis
     """
-    lenTS,noPixels = S2.shape
     m = np.mean(S2,axis=0)
     S2 = S2 - m
     G = np.concatenate([X, B]).transpose();
@@ -25,7 +23,7 @@ def semiparamRegressionRaw(S2, X, B, P):
     S_P = linalg.block_diag(E1,P)
     Pterm = S_P.transpose().dot(S_P)
     # allocate intermediate storage 
-    lambdas= np.linspace(0.1,10,10)
+    lambdas= np.linspace(0.1,1,1)
     GtG = G.transpose().dot(G)
     AIC = np.zeros([len(lambdas),noPixels])
     Z = np.zeros([len(lambdas),noPixels])
@@ -35,74 +33,23 @@ def semiparamRegressionRaw(S2, X, B, P):
         GtGpD = GtG + lambda_i * Pterm;
         GTGpDsG = linalg.solve(GtGpD,G.transpose())
         beta = GTGpDsG.dot(S2)
+        beta_nonparam = beta[noFixedComponents:]
+        # MRF regularization
+        beta_nonparam = pm.pixel_mrf_model(num_knots,num_clusters,beta_nonparam,S2,B,noPixels) 
+        XtX = X.transpose().dot(X)
+        XtXinvXt = linalg.lstsq(XtX, X.transpose())[0]   #linalg.lstsq since X is a singular matrix
+        alpha_param = XtXinvXt.transpose().dot(S2 - B.T.dot(beta_nonparam))
+        beta = np.concatenate([alpha_param, beta_nonparam])
         # compute model statistics
         seqF = G.dot(beta)
         eGlobal = S2 - seqF
         RSS = np.sum(eGlobal ** 2, axis=0)
         df = np.trace(GTGpDsG.dot(G));
-        # covA = (GtGpD)^-1 * GtGpD * (GtGpD)^-1
         covA_1 = linalg.solve(GtGpD,GtG)
         covA = linalg.solve(GtGpD.transpose(),covA_1.transpose()).transpose()
         # covariance matrix of our components
         s_square = RSS / (noTimepoints-df-1)
         # Z-value of our parametric component
-        #z_i = beta[0,:] / np.sqrt(s_square * covA[0,0])
-        Z[i,] = beta[0,:] / np.sqrt(s_square * covA[0,0])
-        # compute AICc
-        AIC_i = np.log(RSS) + (2 * (df+1)) / (noTimepoints-df-2)
-        AIC[i,] = AIC_i
-
-    minAICcIdx = np.argmin(AIC,axis=0)
-    Z = Z.transpose()
-    Z_minAIC = Z[np.arange(Z.shape[0]), minAICcIdx]
-
-    return Z_minAIC
-
-
-def semiparamRegression(S2, X, B, B2, P, P2, noPixels,num_clusters, num_knots):
-    """Apply semiparametric regression framework to imaging data.
-    S: m x n data cube with m time series of length n
-    X: length m vector of discretized parametric function
-    B: non parametric basis
-    P: penalty matrix of non-parametric basis
-    """
-    m = np.mean(S2,axis=0)
-    S2 = S2 - m
-    G = np.concatenate([X, B, B2]).transpose();
-    [noFixedComponents, noTimepoints] = X.shape
-    [nonParamComponents,noTimepoints] = B.shape
-    assert (noFixedComponents == 1), "The hypothesis test only works for a single parametric component."
-    # compute Penalty term
-    E1 = 0 * np.eye(noFixedComponents)
-    S_P = linalg.block_diag(E1,P,P2)
-    Pterm = S_P.transpose().dot(S_P)
-    # allocate intermediate storage 
-    lambdas= np.linspace(0.1,10,10)
-    GtG = G.transpose().dot(G)
-    AIC = np.zeros([len(lambdas),noPixels])
-    Z = np.zeros([len(lambdas),noPixels])
-    for i in range(0,len(lambdas)):
-        # fit model using penalised normal equations
-        lambda_i = lambdas[i]
-        GtGpD = GtG + lambda_i * Pterm;
-        GTGpDsG = linalg.solve(GtGpD,G.transpose())
-        beta = GTGpDsG.dot(S2)
-        #start MRF regularization
-        beta_mrf = beta[noFixedComponents+nonParamComponents :]                       #beta_mrf_before_regularization
-        beta_mrf = pm.pixel_mrf_model(num_knots,num_clusters,beta_mrf,S2,B2,noPixels) #beta_mrf_after_regularization
-        beta = np.concatenate([beta[0:noFixedComponents + nonParamComponents,:], beta_mrf])
-        # compute model statistics
-        seqF = G.dot(beta)
-        eGlobal = S2 - seqF
-        RSS = np.sum(eGlobal ** 2, axis=0)
-        df = np.trace(GTGpDsG.dot(G));
-        # covA = (GtGpD)^-1 * GtGpD * (GtGpD)^-1
-        covA_1 = linalg.solve(GtGpD,GtG)
-        covA = linalg.solve(GtGpD.transpose(),covA_1.transpose()).transpose()
-        # covariance matrix of our components
-        s_square = RSS / (noTimepoints-df-1)
-        # Z-value of our parametric component
-        #z_i = beta[0,:] / np.sqrt(s_square * covA[0,0])
         Z[i,] = beta[0,:] / np.sqrt(s_square * covA[0,0])
         # compute AICc
         AIC_i = np.log(RSS) + (2 * (df+1)) / (noTimepoints-df-2)
