6199a57193f9e88fd764b0ba9ca603a280f323db
nish03 <31392545+nish03@users.noreply.github.com>
2018-03-06 09:39:05 +0100

    Update pixel_mrf_model.py
    Minor changes currently. Refitting parametric component underway

--- semiparamRegression/pixel_mrf_model.py
+++ semiparamRegression/pixel_mrf_model.py
@@ -3,7 +3,7 @@ from sklearn.decomposition import PCA
 from sklearn import mixture
 import opengm
 
-def pixel_mrf_model(num_knots,num_clusters,beta,S2,B,noPixels): 
+def pixel_mrf_model(num_knots,num_clusters,beta,S2,G,noPixels): 
     pca = PCA(n_components=num_knots)
     pca.fit(beta.T)
     var1= numpy.cumsum(numpy.round(pca.explained_variance_ratio_, decimals=3)*100)
@@ -16,12 +16,12 @@ def pixel_mrf_model(num_knots,num_clusters,beta,S2,B,noPixels):
     gmm.fit(beta)
     means  = gmm.means_
     means_inv_PCA = eigenvector_matrix.T.dot(means.T)
-    BtM = B.T.dot(means_inv_PCA)
+    GtM = G.T.dot(means_inv_PCA)
     n_labels_pixels = num_clusters
     n_pixels=noPixels 
     pixel_unaries = numpy.zeros((n_pixels,n_labels_pixels),dtype=numpy.float32)
     for i in range(n_pixels):
-        temp = S2[:,i,numpy.newaxis] - BtM[:]
+        temp = S2[:,i,numpy.newaxis] - GtM[:]
         pixel_unaries[i,:] = numpy.linalg.norm(temp,axis=0)
     pixel_regularizer = opengm.differenceFunction(shape=[n_labels_pixels,n_labels_pixels],norm=1,weight=1.0/n_labels_pixels,truncate=None)
     gm = opengm.graphicalModel([n_labels_pixels]*n_pixels)
