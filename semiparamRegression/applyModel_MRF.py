b01c513fb47a97dd6ca0db350d85f484de058e5e
nish03 <31392545+nish03@users.noreply.github.com>
2018-03-09 15:49:05 +0100

    Update applyModel_MRF.py

--- semiparamRegression/applyModel_MRF.py
+++ semiparamRegression/applyModel_MRF.py
@@ -1,4 +1,5 @@
 #import packages
+#import packages
 import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
@@ -6,20 +7,27 @@ import numpy
 import time 
 import h5py
 import ActivityPatterns as ap
-import basis_mrf as bm
 import pixel_mrf_model as pm
 import region_mrf_model as rm
 import ThermalImagingAnalysis as tai
+import scipy.io
 
 
 # Data, parametric and non-parametric components 
 f = h5py.File("/scratch/p_optim/nish/Master-Thesis/semiparamRegression_2nonparam_MRF/626510_sep.mat", "r")
+g = '/scratch/p_optim/nish/Master-Thesis/Penalties/LearnedPenalties_Gaussian_BSpline_knots_638.mat'
+g = scipy.io.loadmat(g)
+
+
 S = numpy.array(f["S1024"].value)
 T = numpy.array(f["T1024"].value)
-f_P = h5py.File("/scratch/p_optim/nish/Master-Thesis/semiparamRegression_2nonparam_MRF/Penalty_Gaussian_1024fr_2.5Hz_TruncatedWaveletBasis.mat", "r")
-P = f_P["BPdir2"].value        # learned penalty matrix
-P = P.transpose()              # P appears to be stored as transposed version of itself
-B = f_P["B"].value             # basis matrix 
+#f_P = h5py.File("/scratch/p_optim/nish/Master-Thesis/semiparamRegression_2nonparam_MRF/Penalty_Gaussian_1024fr_2.5Hz_TruncatedWaveletBasis.mat", "r")
+#P = f_P["BPdir2"].value        # learned penalty matrix
+#P = P.transpose()              # P appears to be stored as transposed version of itself
+#B = f_P["B"].value             # basis matrix 
+B = g['B'].transpose()
+P = g['BPdir2']
+
 S2 = S[0:1024,]
 T2 = T[0:1024,]
 del S;
@@ -32,8 +40,7 @@ num_knots = P.shape[0]
 num_clusters = 10
 
 #semiparametric regression
-Z = tai.semiparamRegression(S2, X, B, P, num_knots,num_clusters, noPixels)
-Z = abs(Z)
+Z = tai.semiparamRegression(S2, X, B, P, num_knots, num_clusters, noPixels)
 plt.imshow(Z.reshape(640,480).transpose())
 plt.show()
 
@@ -48,3 +55,4 @@ false_negative = len(numpy.where(abs(Z[groundtruth_background,]) >= 5.2)[0])
 true_positive_rate = true_positive / numpy.float32(len(groundtruth_foreground))
 false_positive_rate = false_positive / numpy.float32(len(groundtruth_background))
 accuracy  = (true_positive + true_negative) / numpy.float32(len(groundtruth_background) + len(groundtruth_foreground))
+  
