027537c982e058c83a51fdbfe3cdffb215779b17
nish03 <31392545+nish03@users.noreply.github.com>
2018-03-10 12:25:11 +0100

    Update applyModel_noMRF.py

--- semiparamRegression/applyModel_noMRF.py
+++ semiparamRegression/applyModel_noMRF.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 import ThermalImagingAnalysis as tai
 import ActivityPatterns as ap
 import numpy as np
+from sklearn.metrics import f1_score
 
 pPenalty = "Penalty_Gaussian_1024fr_2.5Hz_TruncatedWaveletBasis.mat"
 #pData = "sep_1072240.mat"
@@ -30,6 +31,24 @@ del T;
 
 X = ap.computeGaussianActivityPattern(np.squeeze(T2)).transpose();
 Z = tai.semiparamRegressionRaw(S2,X,B,P);
+
+Z_true = groundtruthImg.flatten()
+for i in range(len(Z_true)):
+    if Z_true[i] != 0:
+       Z_true[i] = 1
+    else:
+       Z_true[i] = 0
+
+Z_pred = numpy.zeros(len(Z_true))
+for i in range(len(Z_pred)):
+    if Z[i] >= 5.2:
+       Z_pred[i] = 1
+    else:
+       Z_pred[i] = 0
+    
+F1 = f1_score(Z_true, Z_pred, average='binary')
+
+
 
 with h5py.File("result.h5","w") as f:
   d1 = f.create_dataset('Z',data=Z)
