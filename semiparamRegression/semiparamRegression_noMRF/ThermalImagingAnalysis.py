03d58372eebd83babaf460912b1625afc880b6f0
nish03 <31392545+nish03@users.noreply.github.com>
2018-03-16 13:15:53 +0100

    Create ThermalImagingAnalysis.py

---
+++ semiparamRegression/semiparamRegression_noMRF/ThermalImagingAnalysis.py
@@ -0,0 +1,59 @@
+import ActivityPatterns as ap
+import numpy as np
+import h5py
+import scipy.linalg as linalg
+import matplotlib.pyplot as plt
+
+
+
+def semiparamRegressionRaw(S2, X, B, P):
+    """Apply semiparametric regression framework to imaging data.
+    S: m x n data cube with m time series of length n
+    X: length m vector of discretized parametric function
+    B: non parametric basis
+    P: penalty matrix of non-parametric basis
+    """
+    lenTS,noPixels = S2.shape
+    m = np.mean(S2,axis=0)
+    S2 = S2 - m
+    G = np.concatenate([X, B]).transpose();
+    [noFixedComponents, noTimepoints] = X.shape
+    [nonParamComponents,noTimepoints] = B.shape
+    assert (noFixedComponents == 1), "The hypothesis test only works for a single parametric component."
+    # compute Penalty term
+    E1 = 0 * np.eye(noFixedComponents)
+    S_P = linalg.block_diag(E1,P)
+    Pterm = S_P.transpose().dot(S_P)
+    # allocate intermediate storage 
+    lambdas= np.linspace(0.1,10,10)
+    GtG = G.transpose().dot(G)
+    AIC = np.zeros([len(lambdas),noPixels])
+    Z = np.zeros([len(lambdas),noPixels])
+    for i in range(0,len(lambdas)):
+        # fit model using penalised normal equations
+        lambda_i = lambdas[i]
+        GtGpD = GtG + lambda_i * Pterm;
+        GTGpDsG = linalg.solve(GtGpD,G.transpose())
+        beta = GTGpDsG.dot(S2)
+        # compute model statistics
+        seqF = G.dot(beta)
+        eGlobal = S2 - seqF
+        RSS = np.sum(eGlobal ** 2, axis=0)
+        df = np.trace(GTGpDsG.dot(G));
+        # covA = (GtGpD)^-1 * GtGpD * (GtGpD)^-1
+        covA_1 = linalg.solve(GtGpD,GtG)
+        covA = linalg.solve(GtGpD.transpose(),covA_1.transpose()).transpose()
+        # covariance matrix of our components
+        s_square = RSS / (noTimepoints-df-1)
+        # Z-value of our parametric component
+        #z_i = beta[0,:] / np.sqrt(s_square * covA[0,0])
+        Z[i,] = beta[0,:] / np.sqrt(s_square * covA[0,0])
+        # compute AICc
+        AIC_i = np.log(RSS) + (2 * (df+1)) / (noTimepoints-df-2)
+        AIC[i,] = AIC_i
+
+    minAICcIdx = np.argmin(AIC,axis=0)
+    Z = Z.transpose()
+    Z_minAIC = Z[np.arange(Z.shape[0]), minAICcIdx]
+
+    return Z_minAIC
