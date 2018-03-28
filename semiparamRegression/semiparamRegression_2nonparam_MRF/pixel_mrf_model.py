c41756a0e2c0243123723aea216f7ea9adbc8c42
nish03 <31392545+nish03@users.noreply.github.com>
2018-03-29 01:44:28 +0200

    Update pixel_mrf_model.py

--- semiparamRegression/semiparamRegression_2nonparam_MRF/pixel_mrf_model.py
+++ semiparamRegression/semiparamRegression_2nonparam_MRF/pixel_mrf_model.py
@@ -1,23 +1,32 @@
 import numpy
 from math import sqrt
-from sklearn import mixture
 import opengm
+from sklearn import mixture
+import time
 
-
-def pixel_mrf_model(num_knots,num_clusters,beta,S2,G,noPixels): 
+def pixel_mrf_model(num_knots,num_clusters,beta,S2,G,noPixels,lambda_pairwise): 
     alpha_param = beta[0,:].reshape(1,-1)
-    beta_nonparam = beta[1:]
+    beta_nonparam = beta[1:].transpose()
+    datapoints, DIMENSIONS = beta_nonparam.shape
+    start_time = time.time()
     gmm = mixture.GaussianMixture(n_components=num_clusters,covariance_type = 'diag')
-    gmm.fit(beta_nonparam.T)
+    datapoints, DIMENSIONS = beta_nonparam.shape
+    gmm.fit(beta_nonparam)
     means  = gmm.means_
-    GtM = G.dot(means.T)
+    print('GMM elapsed: ' + str(time.time() - start_time) + ' s')
     n_labels_pixels = num_clusters
-    n_pixels= noPixels 
+    n_pixels=noPixels 
     pixel_unaries = numpy.zeros((n_pixels,n_labels_pixels),dtype=numpy.float32)
-    for i in range(n_pixels):
-        for l in range(n_labels_pixels):
-            pixel_unaries[i,l] = numpy.linalg.norm(S2[:,i] - G.dot(numpy.concatenate([alpha_param[:,i], means.T[:,l]])))
-    pixel_regularizer = opengm.differenceFunction(shape=[n_labels_pixels,n_labels_pixels],norm=1,weight=1.0/n_labels_pixels,truncate=None)
+    start_time = time.time()
+    for l in range(n_labels_pixels):
+         mt = means.T[:,l]
+         mt = mt[...,numpy.newaxis]
+         mt2 = numpy.repeat(mt,datapoints,axis=1)
+         cc = numpy.concatenate([alpha_param, mt2])
+         est = S2 - G.dot(numpy.concatenate([alpha_param, mt2]))
+         pixel_unaries[:,l] = numpy.sqrt(numpy.sum(est**2, axis=0))
+    print('UNARIES elapsed: ' + str(time.time() - start_time) + ' s')
+    pixel_regularizer = opengm.differenceFunction(shape=[n_labels_pixels,n_labels_pixels],norm=1,weight=lambda_pairwise,truncate=None)
     gm = opengm.graphicalModel([n_labels_pixels]*n_pixels)
     fids = gm.addFunctions(pixel_unaries)
     gm.addFactors(fids,numpy.arange(n_pixels))
@@ -26,8 +35,10 @@ def pixel_mrf_model(num_knots,num_clusters,beta,S2,G,noPixels):
     gm.addFactors(fid,vis)
     inf_trws=opengm.inference.TrwsExternal(gm)
     visitor=inf_trws.timingVisitor()
+    start_time = time.time()
     inf_trws.infer(visitor)
     argmin=inf_trws.arg()
+    print('MRF elapsed: ' + str(time.time() - start_time) + ' s')
     centroid_labels = numpy.zeros((n_pixels,num_knots))
     centroid_labels = [means[i,:] for i in argmin]
     centroid_labels = numpy.asarray(centroid_labels)
