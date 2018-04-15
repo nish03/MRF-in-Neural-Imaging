3f361efe30f8afaec95f70a9e3a3ca54c9956f5b
nish03 <31392545+nish03@users.noreply.github.com>
2018-04-15 15:07:17 +0200

    Update Evaulation.py

--- semiparamRegression/Evaluation_FFT/Evaulation.py
+++ semiparamRegression/Evaluation_FFT/Evaulation.py
@@ -7,6 +7,8 @@ import time
 import h5py
 import MRF_model as em
 import scipy.io
+from sklearn import mixture
+import scipy.linalg as linalg
 
 #Load raw and signal data
 f = '/projects/p_optim/Nico/SEP_SemiReg/626510_gaussian_50pxDiameter.mat'
@@ -24,15 +26,15 @@ T = f['T1024']
 
 #load basis and penalty
 print "Raw Data"
-f_P = h5py.File("/scratch/p_optim/nish/Master-Thesis/semiparamRegression_2nonparam_MRF/Penalty_Gaussian_1024fr_2.5Hz_TruncatedWaveletBasis.mat", "r")
-P = f_P["BPdir2"].value        # learned penalty matrix
-P = P.transpose()              # P appears to be stored as transposed version of itself
-B = f_P["B"].value             # basis matrix 
+f_P = scipy.io.loadmat("/scratch/p_optim/nish/Master-Thesis/Penalties/LearnedPenalties_Gaussian_BSpline_knots_428.mat")
+P = f_P["BPdir2"]        # learned penalty matrix
+B = f_P["B"].transpose()             # basis matrix 
 num_knots = P.shape[0]
-num_clusters = 10
-lambdas = 0.1
+num_clusters = 12
+
 
 #fft analysis of target pattern
+T = T.flatten()
 y = numpy.fft.fft(T)
 y = y.flatten()
 noElementsToAnalyse = 128
@@ -40,6 +42,13 @@ contributionWeCareAbout = 90.0 / 100.0      # 90 %
 energyContribution = numpy.zeros(noElementsToAnalyse)
 for i in range(1,noElementsToAnalyse):
       energyContribution[i] = sum(abs(y[0:i])) / sum(abs(y[0:512]))
+
+x = numpy.linspace(0, 512, 512)
+plt.plot(x, energyContribution , label = 'Energy contribution', linewidth = 1.0, color='black')
+plt.xlabel(r'Positive spectral coponents',fontweight='bold',fontsize=10)
+plt.ylabel(r'Energy Contribution ',fontweight='bold', fontsize=10)
+plt.legend(prop={'size': 12})
+plt.show()
   
 contributionWeCareAbout = numpy.repeat(90.0/100.0,noElementsToAnalyse)
 val = min(abs(energyContribution - contributionWeCareAbout))
@@ -49,7 +58,15 @@ print "%.2f%% of the energy is contained in the first %d spectral components\n",
 ##########################################################
 #smooth our raw data and compute statistics
 ##########################################################
-S_raw_mrf = em.evaluation_fft_mrf(num_knots,num_clusters, S_raw,B,P, noPixels, lambdas)
+print "Data with raw Signal"
+
+BtB = B.dot(B.T)
+BTBpDsB = linalg.solve(BtB ,B)
+beta = BTBpDsB.dot(S_raw)
+S_raw_noMRF = B.transpose().dot(beta)
+lambda_pairwise = 0
+
+S_raw_mrf = em.evaluation_fft_mrf(num_knots, num_clusters, beta, S_raw, B, noPixels, lambda_pairwise)
 #fft analysis of raw data
 Y_raw = abs(numpy.fft.fft(S_raw))
 Y_raw_mrf = abs(numpy.fft.fft(S_raw_mrf))
@@ -67,7 +84,12 @@ print "-> RAW: FFT Analysis of raw data at relevant spectral components: Y_raw Y
 #smooth the data being superimposed by our target and compute statistics
 ########################################################################
 print "Data with Target Signal" 
-S_signal_MRF = em.evaluation_fft_mrf(num_knots, num_clusters, S_signal, B, P, noPixels, lambdas)
+
+beta = BTBpDsB.dot(S_signal)
+S_signal_noMRF = B.transpose().dot(beta)
+lambda_pairwise = 0
+
+S_signal_MRF = em.evaluation_fft_mrf(num_knots, num_clusters, beta, S_signal, B, noPixels, lambda_pairwise)
 #fft analysis of signal data
 Y_signal = abs(numpy.fft.fft(S_signal))  #fft and take absolute values
 Y_signal_mrf = abs(numpy.fft.fft(S_signal_MRF))   #fft and take absolute values
@@ -88,11 +110,15 @@ print "-> TARGET: FFT Analysis of signal data at selected spectral components: Y
 ######################################################################
 print "Sum of Squares:"
 ss_raw = sum(sum( (S_raw[:,posIdx] - S_signal[:,posIdx])**2 )) / len(posIdx)
-ss_mrf = sum(sum( (S_raw_mrf[:,posIdx] - S_signal_mrf[:,posIdx])**2 )) / len(posIdx)
+ss_mrf = sum(sum( (S_raw_mrf[:,posIdx] - S_signal_MRF[:,posIdx])**2 )) / len(posIdx)
+ss_noMRF = sum(sum( (S_raw_noMRF[:,posIdx] - S_signal_noMRF[:,posIdx])**2 )) / len(posIdx) 
 print "S_raw - S_signal:", ss_raw
 print "S_raw_mrf - S_signal_mrf:", ss_mrf  
+print "S_raw_noMRF - S_signal_noMRF:", ss_noMRF
+print "The value should approach zero if the estimate match, otherwise the mrf model absorbed some energy of our target signal"
 
-print "The last value should approach zero if the estimate match, otherwise the mrf model absorbed some energy of our target signal"
+
+
 
 
 
