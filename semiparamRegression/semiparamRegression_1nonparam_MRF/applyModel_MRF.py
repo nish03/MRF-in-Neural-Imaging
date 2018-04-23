6c0073cc2d67bca131ed86897fbd832e8ec6ddd8
nish03 <31392545+nish03@users.noreply.github.com>
2018-04-23 03:15:11 +0200

    Update applyModel_MRF.py

--- semiparamRegression/semiparamRegression_1nonparam_MRF/applyModel_MRF.py
+++ semiparamRegression/semiparamRegression_1nonparam_MRF/applyModel_MRF.py
@@ -75,3 +75,48 @@ for i in range(len(Z_pred)):
   
 
 F1 = f1_score(Z_true, Z_pred, average='binary')
+
+#MRF on Z
+n_labels = 2
+n_pixels=noPixels 
+threshold = 5.2
+pixel_unaries = numpy.zeros((n_pixels,n_labels),dtype=numpy.float32)
+for l in range(n_pixels):
+    pixel_unaries[l,0] = Z[l,] - threshold
+    pixel_unaries[l,1] = threshold - Z[l,]
+
+pixel_regularizer = opengm.differenceFunction(shape=[n_labels,n_labels],norm=1,weight=2,truncate=None)
+gm = opengm.graphicalModel([n_labels]*n_pixels)
+fids = gm.addFunctions(pixel_unaries)
+gm.addFactors(fids,numpy.arange(n_pixels))
+fid = gm.addFunction(pixel_regularizer)
+vis = opengm.secondOrderGridVis(640,480)
+gm.addFactors(fid,vis)
+inf_trws=opengm.inference.TrwsExternal(gm)
+visitor=inf_trws.timingVisitor()
+inf_trws.infer(visitor)
+Z_new =inf_trws.arg()
+true_positive =  len(numpy.where(abs(Z_new[groundtruth_foreground,]) >= 1)[0])                                  
+false_positive = len(numpy.where(abs(Z_new[groundtruth_foreground,]) < 1)[0])
+true_negative = len(numpy.where(abs(Z_new[groundtruth_background,]) < 1)[0])
+false_negative = len(numpy.where(abs(Z_new[groundtruth_background,]) >= 1)[0])
+true_positive_rate = true_positive / numpy.float32(len(groundtruth_foreground))
+false_positive_rate = false_positive / numpy.float32(len(groundtruth_background))
+accuracy  = (true_positive + true_negative) / numpy.float32(len(groundtruth_background) + len(groundtruth_foreground))
+Z_true = groundtruthImg.flatten()
+for i in range(len(Z_true)):
+    if Z_true[i] != 0:
+       Z_true[i] = 1
+    else:
+       Z_true[i] = 0
+
+
+Z_pred = numpy.zeros(len(Z_true))
+for i in range(len(Z_pred)):
+    if Z_new[i] >= 1:
+       Z_pred[i] = 1.0
+    else:
+       Z_pred[i] = 0.0
+  
+
+F1 = f1_score(Z_true, Z_pred, average='binary')
