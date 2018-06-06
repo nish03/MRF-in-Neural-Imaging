eceff87ee559d5a653604c4fb93d4e3369653421
nish03 <31392545+nish03@users.noreply.github.com>
2018-06-06 11:48:03 +0200

    Create applyTAI_SRVCM.py

---
+++ semiparamRegression/semiparamRegression_VCM_MRF/applyTAI_SRVCM.py
@@ -0,0 +1,30 @@
+import ThermalImagingAnalysis as tai
+import ActivityPatterns as ap
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+
+
+# load data
+f_P = h5py.File("/scratch/p_optim/nish/Master-Thesis/Penalties/Penalty_Gaussian_1024fr_2.5Hz_TruncatedWaveletBasis.mat","r")
+f = h5py.File("/scratch/p_optim/nish/Master-Thesis/semiparamRegression_2nonparam_MRF/626510_sep.mat", "r")
+S = np.array(f["seq"].value).transpose()
+T = np.squeeze(np.array(f["T"].value))
+f.close()
+S2 = S[0:1024,]
+T2 = T[0:1024,]
+P = f_P["BPdir2"].value   # learned penalty matrix
+print('[INFO] P is being transposed\n')
+P = P.transpose() # P appears to be stored as transposed version of itself
+B = f_P["B"].value # basis matrix
+val = ap.computeBoxcarActivityPattern(T,sigma=30)
+val_neg,vp = val.nonzero()
+# REGRESSION ANALYSIS
+X = ap.computeGaussianActivityPattern(np.squeeze(T2)).transpose();
+start_time = time.time()
+F = tai.semiparamRegressio_VCM(S2,T2,B,P);
+elapsed_time = time.time() - start_time
+print('elapsed (CPU): ' + str(elapsed_time) + ' s')
