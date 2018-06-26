e1a320ddcb04175bbf3657de83656856d839fbed
nish03 <31392545+nish03@users.noreply.github.com>
2018-06-26 15:27:37 +0200

    Update ThermalImagingAnalysis.py

--- semiparamRegression/semiparamRegression_beta/ThermalImagingAnalysis.py
+++ semiparamRegression/semiparamRegression_beta/ThermalImagingAnalysis.py
@@ -11,6 +11,61 @@ from sklearn import mixture
 import opengm
 from scipy.linalg import norm
 from numpy.linalg import lstsq
+from numpy.linalg import inv
+
+
+
+
+
+def semiparamRegression_noMRF(S2, X, B, P):
+    """Apply semiparametric regression framework to imaging data.
+    S: m x n data cube with m time series of length n
+    X: length m vector of discretized parametric function
+    B: non parametric basis
+    P: penalty matrix of non-parametric basis
+    """
+    lenTS,noPixels = S2.shape
+    m = np.mean(S2,axis=0)
+    S2 = S2 - m
+    G = np.concatenate([X, B]).transpose();
+    [noFixedComponents, noTimepoints] = X.shape
+    [nonParamComponents,noTimepoints] = B.shape
+    assert (noFixedComponents == 1), "The hypothesis test only works for a single parametric component."
+    # compute Penalty term
+    E1 = 0 * np.eye(noFixedComponents)
+    S_P = linalg.block_diag(E1,P)
+    Pterm = S_P.transpose().dot(S_P)
+    # allocate intermediate storage 
+    lambdas= np.linspace(0.1,10,10)
+    GtG = G.transpose().dot(G)
+    AIC = np.zeros([len(lambdas),noPixels])
+    Z = np.zeros([len(lambdas),noPixels])
+    for i in range(0,len(lambdas)):
+        # fit model using penalised normal equations
+        lambda_i = lambdas[i]
+        GtGpD = GtG + lambda_i * Pterm;
+        GTGpDsG = linalg.solve(GtGpD,G.transpose())
+        beta = GTGpDsG.dot(S2)
+        # compute model statistics
+        seqF = G.dot(beta)
+        eGlobal = S2 - seqF
+        RSS = np.sum(eGlobal ** 2, axis=0)
+        df = np.trace(GTGpDsG.dot(G));
+        covA_1 = linalg.solve(GtGpD,GtG)
+        covA = linalg.solve(GtGpD.transpose(),covA_1.transpose()).transpose()
+        # covariance matrix of our components
+        s_square = RSS / (noTimepoints-df-1)
+        # Z-value of our parametric component
+        Z[i,] = beta[0,:] / np.sqrt(s_square * covA[0,0])
+        # compute AICc
+        AIC_i = np.log(RSS) + (2 * (df+1)) / (noTimepoints-df-2)
+        AIC[i,] = AIC_i
+
+    minAICcIdx = np.argmin(AIC,axis=0)
+    Z = Z.transpose()
+    Z_minAIC = Z[np.arange(Z.shape[0]), minAICcIdx]
+    return Z_minAIC
+
 
 
 def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
@@ -52,7 +107,7 @@ def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
             mt = means.T[:,l]
             mt = mt[...,np.newaxis]
             mt2 = np.repeat(mt,noPixels,axis=1)
-            Y_gmm = B.transpose().dot(mt2)
+            Y_gmm = B.transpose().dot(inv(P).dot(mt2))
             beta_refit = GTGpDsG.dot(S2 - Y_gmm)
             # compute model statistics
             seqF = G.dot(beta_refit)
