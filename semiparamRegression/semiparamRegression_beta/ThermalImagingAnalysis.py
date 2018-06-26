9cb6aaddfc424031850c0fd7e850ba07073c4a21
nish03 <31392545+nish03@users.noreply.github.com>
2018-06-26 11:32:28 +0200

    Update ThermalImagingAnalysis.py

--- semiparamRegression/semiparamRegression_beta/ThermalImagingAnalysis.py
+++ semiparamRegression/semiparamRegression_beta/ThermalImagingAnalysis.py
@@ -33,7 +33,12 @@ def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
     lambdas= [1] #np.linspace(0.1,10,10)
     GtG = G.transpose().dot(G)
     AIC = np.zeros([len(lambdas),noPixels])
-    Z = np.zeros([num_clusters,noPixels])
+
+
+    '''
+    prepare potential values for our MRF
+    '''
+    Z = np.zeros([num_clusters,len(lambdas),noPixels])
     for i in range(0,len(lambdas)):
         # fit model using penalised normal equations
         lambda_i = lambdas[i]
@@ -59,39 +64,34 @@ def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
             # covariance matrix of our components
             s_square = RSS / (noTimepoints-df-1)
             # refit Z-value of our parametric component
-            Z[i,] = beta_refit[0,:] / np.sqrt(s_square * covA[0,0])
-        #unary potential
-        gm = opengm.graphicalModel([num_clusters]*noPixels)
-        unary_potential = np.zeros([noPixels, num_clusters],dtype=np.float32)
-        unary_potential[:,i] = 1 / Z[i,]
-        fids = gm.addFunctions(unary_potential)
-        gm.addFactors(fids,np.arange(noPixels))
-        #pairwise potential 
-        pairwise_potential = np.zeros(num_clusters*num_clusters,dtype=np.float32).reshape(num_clusters,num_clusters)           
-        for l in range(num_clusters):             
-            for k in range(num_clusters):                    
-                pairwise_potential[l,k] = lambda_pairwise*norm(means[l,:] - means[k,:])          
-        fid = gm.addFunction(pairwise_potential)
-        vis = opengm.secondOrderGridVis(640,480)
-        gm.addFactors(fid,vis)
-        inf_trws = opengm.inference.TrwsExternal(gm)   
-        visitor=inf_trws.timingVisitor()
-        inf_trws.infer(visitor)
-        argmin=inf_trws.arg()
-        pbeta_mrf = np.zeros((noPixels,num_clusters))
-        pbeta_mrf = [means[i,:] for i in argmin]
-        pbeta_mrf = np.asarray(pbeta_mrf).transpose()
-        beta_mrf  = lstsq(P,pbeta_mrf)
-        beta_mrf = np.asarray(beta_mrf[0])
-        Y_mrf = B.transpose().dot(beta_mrf)
-        beta_refit = GTGpDsG.dot(S2 - Y_mrf)
-        # compute model statistics
-        seqF = G.dot(beta_refit)
-        eGlobal = S2 - Y_mrf - seqF
-        RSS = np.sum(eGlobal ** 2, axis=0)
-        # covariance matrix of our components
-        s_square = RSS / (noTimepoints-df-1)
-        # Z-value of our parametric component
-        Z = beta_refit[0,:] / np.sqrt(s_square * covA[0,0])
-        return Z
+            Z[l,i,] = beta_refit[0,:] / np.sqrt(s_square * covA[0,0])
 
+
+    '''
+    MRF inference
+    '''
+    # for each cluster, pixel: compute Z_min (marginalize lambda)
+    Zcp = Z.min(axis=1)
+    #unary potential
+    gm = opengm.graphicalModel([num_clusters]*noPixels)
+    unary_potential = np.zeros([noPixels, num_clusters],dtype=np.float32)
+    for l in range(0,num_clusters):
+    	unary_potential[:,l] = 1 / Zcp[l,]
+    fids = gm.addFunctions(unary_potential)
+    gm.addFactors(fids,np.arange(noPixels))
+    #pairwise potential 
+    pairwise_potential = np.zeros(num_clusters*num_clusters,dtype=np.float32).reshape(num_clusters,num_clusters)       
+    for l in range(num_clusters):         
+        for k in range(num_clusters):            
+            pairwise_potential[l,k] = lambda_pairwise*norm(means[l,:] - means[k,:],ord=2)      
+    fid = gm.addFunction(pairwise_potential)
+    vis = opengm.secondOrderGridVis(640,480)
+    gm.addFactors(fid,vis)
+    inf_trws = opengm.inference.TrwsExternal(gm)   
+    visitor=inf_trws.timingVisitor()
+    inf_trws.infer(visitor)
+    argmin=inf_trws.arg()
+    # we dont need to recompute Z as we already have the Z value for each pixel and label
+    for j in range(noPixels):
+        Z_mrf[j] = Zcp[argmin[j],j]
+    return Z_mrf
