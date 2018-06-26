f079b6a7b1f1aac661d72fd00b4240ac69acd4b8
nish03 <31392545+nish03@users.noreply.github.com>
2018-06-26 23:51:06 +0200

    Update ThermalImagingAnalysis.py

--- semiparamRegression/semiparamRegression_beta/ThermalImagingAnalysis.py
+++ semiparamRegression/semiparamRegression_beta/ThermalImagingAnalysis.py
@@ -13,10 +13,6 @@ from scipy.linalg import norm
 from numpy.linalg import lstsq
 from numpy.linalg import inv
 
-
-
-
-
 def semiparamRegression_noMRF(S2, X, B, P):
     """Apply semiparametric regression framework to imaging data.
     S: m x n data cube with m time series of length n
@@ -36,7 +32,7 @@ def semiparamRegression_noMRF(S2, X, B, P):
     S_P = linalg.block_diag(E1,P)
     Pterm = S_P.transpose().dot(S_P)
     # allocate intermediate storage 
-    lambdas= np.linspace(0.1,10,10)
+    lambdas= [1] #np.linspace(0.1,10,10)
     GtG = G.transpose().dot(G)
     AIC = np.zeros([len(lambdas),noPixels])
     Z = np.zeros([len(lambdas),noPixels])
@@ -88,8 +84,6 @@ def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
     lambdas= [1] #np.linspace(0.1,10,10)
     GtG = G.transpose().dot(G)
     AIC = np.zeros([len(lambdas),noPixels])
-
-
     '''
     prepare potential values for our MRF
     '''
@@ -101,13 +95,13 @@ def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
         GTGpDsG = linalg.solve(GtGpD,G.transpose())
         beta = GTGpDsG.dot(S2)
         gmm = mixture.GaussianMixture(n_components=num_clusters,covariance_type = 'diag')
-        gmm.fit(P.dot(beta[1:,]).T)
+        gmm.fit(beta[1:,].T)
         means = gmm.means_
         for l in range(0,num_clusters):
             mt = means.T[:,l]
             mt = mt[...,np.newaxis]
             mt2 = np.repeat(mt,noPixels,axis=1)
-            Y_gmm = B.transpose().dot(inv(P).dot(mt2))
+            Y_gmm = B.transpose().dot(mt2)
             beta_refit = GTGpDsG.dot(S2 - Y_gmm)
             # compute model statistics
             seqF = G.dot(beta_refit)
@@ -121,7 +115,6 @@ def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
             # refit Z-value of our parametric component
             Z[l,i,] = beta_refit[0,:] / np.sqrt(s_square * covA[0,0])
 
-
     '''
     MRF inference
     '''
@@ -131,7 +124,7 @@ def semiparamRegression(S2, X, B, P, num_clusters, noPixels, lambda_pairwise):
     gm = opengm.graphicalModel([num_clusters]*noPixels)
     unary_potential = np.zeros([noPixels, num_clusters],dtype=np.float32)
     for l in range(0,num_clusters):
-    	unary_potential[:,l] = 1 / Zcp[l,]
+    	unary_potential[:,l] = -1 * abs(Zcp[l,])
     fids = gm.addFunctions(unary_potential)
     gm.addFactors(fids,np.arange(noPixels))
     #pairwise potential 
